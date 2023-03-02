31e691c697bd0a31abae52ff8a25a073636ca83b
Piyush Jain <piyushjain@duck.com>
2023-03-02 10:35:31 -0800

    Added comments

--- bundle.sh
+++ bundle.sh
@@ -2,8 +2,12 @@
 
 rm -rf dist
 mkdir dist
+
 pip install --platform manylinux2014_x86_64 --implementation cp --only-binary=:all: -r requirements.txt -t dist
-pip install -I openai -t dist
+#openai doesn't have a binary distribution, so need a separate install
+pip install -I openai -t dist 
+
+#remove extraneous bits from installed packages
 rm -r dist/*.dist-info
 cp config.py memory.py chain.py main.py dist/
 cd dist && zip -r lambda.zip *
