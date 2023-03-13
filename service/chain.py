157f8b0bee1bdb61b0199e1235a8105ad7e2603a
Piyush Jain <piyushjain@duck.com>
2023-03-13 12:24:00 -0700

    Refactored memory as per changes in langchain 0.0.107

--- service/chain.py
+++ service/chain.py
@@ -1,13 +1,49 @@
+from typing import List, Tuple
+from uuid import uuid4
 from langchain import ConversationChain, OpenAI
-from memory import ConversationBufferStoreMemory, DynamoDBStore
+from memory import ConversationBufferPersistentStoreMemory, DynamoDBMessageStore, PersistentChatMessageHistory
 
 import config
 
-def run(api_key: str, session_id: str, prompt: str) -> str:
-    memory = ConversationBufferStoreMemory(
-        buffer=DynamoDBStore(table_name=config.config.DYNAMODB_TABLE_NAME),
+def run(api_key: str, session_id: str, prompt: str) -> Tuple[str, str]:
+    """This is the main function that executes the prediction chain.
+    Updating this code will change the predictions of the service.
+    Current implementation creates a new session id for each run, client
+    should pass the returned session id in the next execution run, so the
+    conversation chain can load message context from previous execution.
+
+    Args:
+        api_key: api key for the LLM service, OpenAI used here
+        session_id: session id from the previous execution run, pass blank for first execution
+        prompt: prompt question entered by the user
+
+    Returns:
+        The prediction from LLM and the session id
+    """
+
+    if not session_id:
+        session_id = str(uuid4())
+    
+    store = DynamoDBMessageStore(
+        table_name=config.config.DYNAMODB_TABLE_NAME,
         session_id=session_id
-    )   
+    )
+    
+    # Maintains immutable sessions
+    # If previous session was present, create
+    # a new session and copy messages, and 
+    # generate a new session_id 
+    messages = store._read()
+    if messages:
+        session_id = str(uuid4())
+        store = DynamoDBMessageStore(
+            table_name=config.config.DYNAMODB_TABLE_NAME,
+            session_id=session_id
+        )
+        store._write(session_id, messages)
+    
+    chat_history = PersistentChatMessageHistory(store=store)
+    memory = ConversationBufferPersistentStoreMemory(chat_memory=chat_history)   
     
     llm = OpenAI(temperature=0.9, openai_api_key=api_key)
     conversation = ConversationChain(
@@ -17,4 +53,4 @@ def run(api_key: str, session_id: str, prompt: str) -> str:
     )
     response = conversation.predict(input=prompt)
     
-    return response, memory.session_id
+    return response, session_id
--- service/memory.py
+++ service/memory.py
@@ -1,11 +1,12 @@
-from typing import Any, Dict, List, Optional
-from uuid import uuid4
+from abc import ABC, abstractmethod
+from typing import Any, Dict, List
 
 import boto3
 from botocore.exceptions import ClientError
 
-from langchain.chains.base import Memory
-from langchain.chains.conversation.memory import _get_prompt_input_key
+from langchain.memory.utils import get_buffer_string
+from langchain.schema import BaseMessage, HumanMessage, AIMessage, SystemMessage, ChatMessage
+from langchain.memory.chat_memory import BaseChatMemory, ChatMessageHistory
 
 from pydantic import BaseModel
 
@@ -13,73 +14,144 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-class DynamoDBStore:
-    def __init__(self, table_name):
-        self.table_name = table_name
-        self.ddb = boto3.client("dynamodb")
 
-    def read(self, session_id: str) -> str:
-        if not session_id:
-            return ""
+class MessageStore(ABC):
+    session_id: str
+
+    @abstractmethod
+    def read(self) -> List[BaseMessage]:
+        ...
+
+    @abstractmethod
+    def add_user_message(self, message: HumanMessage) -> None:
+        ...
+
+    @abstractmethod
+    def add_ai_message(self, message: AIMessage) -> None:
+        ...
+
+    @abstractmethod
+    def clear(self) -> None:
+        ...
+
+class DynamoDBMessageStore(MessageStore):
+    def __init__(self, table_name, session_id: str):
+        client = boto3.resource("dynamodb")
+        self.table = client.Table(table_name)
+        self.session_id = session_id
+
+    def _read(self) -> List[Dict]:
         try: 
-            response = self.ddb.get_item(
-                TableName=self.table_name, 
-                Key={'SessionId': {'S': session_id}}
+            response = self.table.get_item(
+                Key={'SessionId': self.session_id}
             )
         except ClientError as error:
             if error.response['Error']['Code'] == 'ResourceNotFoundException':
-                logger.warn("No record found with session id: %s", session_id)
+                logger.warn("No record found with session id: %s", self.session_id)
             else:
                 logger.error(error)
         
         if response and 'Item' in response:
-            return response['Item']['History']['S']
+            return response['Item']['History']
         
-        return ''
+        return []
 
-    def write(self, session_id: str, history: str):
-        if not session_id:
-            return
+    def read(self) -> List[BaseMessage]:
+        items = self._read()
+        messages = []
+        for item in items:
+            role = item["role"]
+            content = item["content"]
+            if role == 'human':
+                messages.append(HumanMessage(content=content))
+            elif role == 'ai':
+                messages.append(AIMessage(content=content))
+            elif role == 'system':
+                messages.append(SystemMessage(content=content))
+            else:
+                messages.append(ChatMessage(content=content, role=role))
+
+        return messages
+    
+    def add_user_message(self, message: HumanMessage) -> None:
+        self._add_message(message)
+
+    def add_ai_message(self, message: AIMessage) -> None:
+       self._add_message(message)
+
+    def _add_message(self, message: BaseMessage) -> None:
+        messages = self._read()
+        if isinstance(message, HumanMessage):
+            role = "human"
+        elif isinstance(message, AIMessage):
+            role = "ai"
+        elif isinstance(message, SystemMessage):
+            role = "system"
+        elif isinstance(message, ChatMessage):
+            role = message.role
+        else:
+            raise ValueError(f"Got unsupported message type: {message}")
+        messages.append({"role": role, "content": message.content})
+        self._write(self.session_id, messages)
+
+    def _write(self, session_id: str, messages: List[Dict]):
         try:
-            self.ddb.put_item(
-                TableName=self.table_name,
+            self.table.put_item(
                 Item={
-                    'SessionId': {
-                        'S': session_id
-                    },
-                    'History': {
-                        'S': history
-                    }
+                    'SessionId': session_id,
+                    'History': messages
                 }
             )
         except ClientError as err:
             logger.error(err)
-
-    def clear(self, session_id: str):
-        if not session_id:
-            return
+    
+    def clear(self):
         try:
-            self.ddb.delete_item(
-                TableName=self.table_name,
-                Key={
-                    'SessionId': {'S': session_id}
-                }
+            self.table.delete_item(
+                Key={'SessionId': self.session_id}
             )
         except ClientError as err:
             logger.error(err)
 
+class PersistentChatMessageHistory(ChatMessageHistory):
+    store: MessageStore
 
-class ConversationBufferStoreMemory(Memory, BaseModel):
+    class Config:
+        arbitrary_types_allowed = True
+    
+    @property
+    def messages(self) -> List[BaseMessage]:
+        self.store.read()
+
+    def add_user_message(self, message: str) -> None:
+        self.store.add_user_message(HumanMessage(content=message))
+
+    def add_ai_message(self, message: str) -> None:
+        self.store.add_ai_message(AIMessage(content=message))
+
+    def clear(self) -> None:
+        self.store.clear()
+
+
+class ConversationBufferPersistentStoreMemory(BaseChatMemory, BaseModel):
     """Buffer for storing conversation memory."""
 
+    chat_memory: PersistentChatMessageHistory
     human_prefix: str = "Human"
-    """Prefix to use for AI generated responses."""
     ai_prefix: str = "AI"
-    buffer: DynamoDBStore
-    output_key: Optional[str] = None
-    input_key: Optional[str] = None
     memory_key: str = "history"  #: :meta private:
-    session_id: str = ""
+
+    @property
+    def buffer(self) -> Any:
+        """String buffer of memory."""
+        if self.return_messages:
+            return self.chat_memory.messages
+        else:
+            return get_buffer_string(
+                self.chat_memory.messages,
+                human_prefix=self.human_prefix,
+                ai_prefix=self.ai_prefix,
+            )
 
     @property
     def memory_variables(self) -> List[str]:
@@ -88,42 +160,7 @@ class ConversationBufferStoreMemory(Memory, BaseModel):
         :meta private:
         """
         return [self.memory_key]
-
-    def load_memory_variables(self, inputs: Dict[str, Any]) -> Dict[str, str]:
+   
+    def load_memory_variables(self, inputs: Dict[str, Any]) -> Dict[str, Any]:
         """Return history buffer."""
-        return {self.memory_key: self.buffer.read(self.session_id)}
-
-    def save_context(self, inputs: Dict[str, Any], outputs: Dict[str, str]) -> None:
-        """Save context from this conversation to buffer."""
-        if self.input_key is None:
-            prompt_input_key = _get_prompt_input_key(inputs, self.memory_variables)
-        else:
-            prompt_input_key = self.input_key
-        if self.output_key is None:
-            if len(outputs) != 1:
-                raise ValueError(f"One output key expected, got {outputs.keys()}")
-            output_key = list(outputs.keys())[0]
-        else:
-            output_key = self.output_key
-        human = f"{self.human_prefix}: " + inputs[prompt_input_key]
-        ai = f"{self.ai_prefix}: " + outputs[output_key]
-        history = self.buffer.read(self.session_id)
-        if history:
-            history += "\n"
-        
-        # Keeps each conversation immutable, this session id
-        # should be returned to the user
-        self.session_id = self.create_session_id()
-
-        self.buffer.write(
-            session_id=self.session_id,
-            history=history + "\n".join([human, ai])
-        ) 
-
-    def create_session_id(self) -> str:
-        """Creates a new session id"""
-        return str(uuid4())
-
-    def clear(self) -> None:
-        """Clear memory contents."""
-        self.buffer.clear(self.session_id)
+        return {self.memory_key: self.buffer}
